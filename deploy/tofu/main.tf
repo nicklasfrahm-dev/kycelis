52f01f80387b4a581dc96b51c63a5671203bd037
Nicklas Frahm <nicklas.frahm@gmail.com>
2024-10-08 22:24:42 +0200

    fix(ci): add deploy job

--- deploy/tofu/main.tf
+++ deploy/tofu/main.tf
@@ -1,3 +1,8 @@
+resource "google_service_account" "kycelisd" {
+  account_id   = "kycelisd"
+  display_name = "A service account for the kycelisd service."
+}
+
 resource "google_cloud_run_v2_service" "kycelisd" {
   project  = var.GOOGLE_PROJECT_ID
   name     = "kycelisd"
@@ -6,12 +11,20 @@ resource "google_cloud_run_v2_service" "kycelisd" {
   ingress = "INGRESS_TRAFFIC_ALL"
 
   template {
+    service_account = google_service_account.kycelisd.email
+
     containers {
       image = var.CONTAINER_IMAGE
-      resources {
-        limits = {
-          cpu    = "10m"
-          memory = "128Mi"
+
+      liveness_probe {
+        failure_threshold     = 3
+        initial_delay_seconds = 0
+        period_seconds        = 10
+        timeout_seconds       = 1
+
+        http_get {
+          path = "/health"
+          port = 8080
         }
       }
     }
